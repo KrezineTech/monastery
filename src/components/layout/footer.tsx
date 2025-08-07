9abe681f93305bd22dbe8d54c4e351c03a9742c2
Krezine Tech <krezine.dev@gmail.com>
2025-08-07 16:21:51 +0000

    make it more less

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -32,7 +32,7 @@ const socialLinks = [
 export function Footer() {
   return (
     <footer className="w-full mb-4">
-      <div className="w-[96%] mx-auto pt-12 pb-8 text-foreground/80 rounded-[26px] border border-border/40 bg-[#F6F6F3]/80 backdrop-blur-lg px-8">
+      <div className="w-[96%] mx-auto pt-12 pb-6 text-foreground/80 rounded-[26px] border border-border/40 bg-[#F6F6F3]/80 backdrop-blur-lg px-8">
         <div className="mb-12 text-center">
             <Link href="/">
                 <Image 
