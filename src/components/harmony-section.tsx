be381d977e8ce4fead6e6dbfdfda6aa6f2371bc0
Krezine Tech <krezine.dev@gmail.com>
2025-08-10 07:45:47 +0000

    make texts in just 2 lines and please add image as given image and link

--- src/components/harmony-section.tsx
+++ src/components/harmony-section.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function HarmonySection() {
   return (
@@ -18,7 +19,7 @@ export function HarmonySection() {
                 data-ai-hint="pink texture"
               />
             </span>{' '}
-            high-performance ingredients and exceptionally simple{' '}
+            high-performance ingredients and <br/> exceptionally simple{' '}
             <span className="inline-block align-middle -mt-2 mx-1">
               <Image
                 src="https://cdn.shopify.com/s/files/1/0723/1376/6028/files/WEBSITE_REVAMP_DESIGNS-21.webp?v=1754619697"
@@ -39,6 +40,16 @@ export function HarmonySection() {
                 data-ai-hint="woman skincare"
               />
             </span>{' '}
+            <Link href="/shop" className="inline-block align-middle -mt-2 mx-1">
+                <Image
+                    src="https://cdn.shopify.com/s/files/1/0723/1376/6028/files/WEBSITE_REVAMP_DESIGNS-23.webp?v=1754619697"
+                    alt="skincare routine"
+                    width={60}
+                    height={60}
+                    className="rounded-full"
+                    data-ai-hint="skincare routine"
+                />
+            </Link>
             skincare routines.
           </h2>
         </div>
