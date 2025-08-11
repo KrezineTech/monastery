3b9ab07709ec1036abd5952ed76e0a81fbb2aae1
Krezine Tech <krezine.dev@gmail.com>
2025-08-11 17:55:45 +0000

    https://cdn.shopify.com/s/files/1/0723/1376/6028/files/Serum_copy.webp?v

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ import { ReviewSection } from '@/components/review-section';
 
 const categories: Category[] = [
   { name: 'Moisturizers', image: 'https://cdn.shopify.com/s/files/1/0723/1376/6028/files/Untitled-1.webp?v=1754914138', mobileImage: 'https://cdn.shopify.com/s/files/1/0723/1376/6028/files/Untitled-1.webp?v=1754914138', aiHint: 'skincare moisturizer' },
-  { name: 'Serum', image: 'https://cdn.shopify.com/s/files/1/0723/1376/6028/files/Serum.webp?v=1754933298', aiHint: 'skincare serum' },
+  { name: 'Serum', image: 'https://cdn.shopify.com/s/files/1/0723/1376/6028/files/Serum_copy.webp?v=1754934890', aiHint: 'skincare serum' },
   { name: 'Essence', image: 'https://cdn.shopify.com/s/files/1/0723/1376/6028/files/Toner.webp?v=1754934725', aiHint: 'skincare essence' },
 ];
 
