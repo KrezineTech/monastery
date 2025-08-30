ca95edde010ede6973055e6cf4b8e6687838a5d0
Krezine Tech <krezine.dev@gmail.com>
2025-08-30 16:04:59 +0000

    not i hase 46px fotnsize so please make it 32 just for blog details page

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -57,7 +57,7 @@ export default function BlogPostPage() {
 
     <div className="w-[96%] mx-auto px-4 py-12 sm:py-16">
       <article>
-        <div className="prose prose-lg mx-auto text-foreground/90 leading-relaxed">
+        <div className="prose prose-lg mx-auto text-foreground/90 leading-relaxed max-w-none">
           <p className="whitespace-pre-line">{blog.content}</p>
         </div>
       </article>
